da30aaf6ca8140513b2299b7e5ac25b8486825f0
Jaeboong <cbkjh0225@gmail.com>
2024-05-23 16:39:12 +0900

    sequlize db add

--- board1.js
+++ board1.js
@@ -4,6 +4,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const asyncHandler = require('express-async-handler');
+const { sequelize, User } = require('./src/index');  // Sequelize 인스턴스 및 모델 가져오기
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -25,6 +26,30 @@ app.use('/board', asyncHandler(boardRoutes));
 app.use((req, res, next) => {
   res.status(404).send('Page not found');
 });
+
+const initializeApp = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+    await sequelize.sync({ force: true });  // 데이터베이스 동기화 (force: true로 설정하여 기존 테이블을 삭제하고 다시 생성)
+    console.log('Database synchronized');
+
+    // 목업 데이터 삽입
+    await User.bulkCreate([
+      { name: 'Alice2', age: 30, married: false },
+      { name: 'Bob2', age: 40, married: true },
+      { name: 'Charlie2', age: 25, married: false },
+    ]);
+    console.log('Mock data inserted');
+
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+
+initializeApp();
+
+
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
---
+++ src/index.js
@@ -0,0 +1,31 @@
+// src/index.js
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const env = process.env.NODE_ENV || 'development';
+const config = require('../config/config.json')[env];
+const db = {};
+
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
+const User = require('./models/user');
+User.init(sequelize);
+db.User = User;
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
