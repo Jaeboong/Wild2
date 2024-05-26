20caa0b2db4c570e5056e1e19c631005ef1d934a
8mul <164719127+8mul@users.noreply.github.com>
2024-05-26 19:51:15 +0900

    fix_path

--- models/announcement.js
+++ models/announcement.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../config/db');
+const sequelize = require('../src/index');
 
 const Announcement = sequelize.define('Announcement', {
   title: {
--- models/board.js
+++ models/board.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../config/db');
+const sequelize = require('../src/index');
 
 const Board = sequelize.define('Board', {
   title: {
