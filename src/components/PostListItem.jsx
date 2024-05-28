8b7a843368284f85cbb40bec2d7fe2b193da8553
bogyeong11 <chlqhrud0511@naver.com>
2024-05-28 17:28:05 +0900

    git push by gw

---
+++ src/components/PostListItem.jsx
@@ -0,0 +1,36 @@
+import { findByLabelText, getSuggestedQuery } from "@testing-library/react";
+import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+    width: calc(100% - 32px);
+    padding: 16px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+    border: 1px solid grey;
+    border-radius: 8px;
+    cursor: pointer;
+    background: white;
+    :hover {
+        background: lightgrey;
+    }
+`;
+
+const TitleText = styled.p`
+    font-size: 20px;
+    font-weight: 500;
+`;
+
+function PostListIte(props) {
+    const { post, onClick } = props;
+    
+    return (
+        <Wrapper onClick={onClick}>
+            <TitleText>{post.title}</TitleText>
+        </Wrapper>
+    );
+}
+
+export default PostListItem;
